ef76b54573411f42e5043c3f5ecceae91e4e83e4
Belverk070 <vometlin@gmail.com>
2022-02-16 20:31:01 +0300

    Added PlaceModel

---
+++ MyPlacesApp/PlaceModel.swift
@@ -0,0 +1,30 @@
+//
+//  PlaceModel.swift
+//  MyPlacesApp
+//
+//  Created by Василий Метлин on 16.02.2022.
+//
+
+import UIKit
+
+struct Place {
+    
+    var name: String
+    var locaction: String
+    var type: String
+    var image: String
+    
+    static let arrayOfCafe = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
+    
+    static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in arrayOfCafe {
+            places.append(Place(name: place, locaction: "Moscow", type: "Cafe", image: place))
+        }
+        
+        return places
+    }
+    
+}
--- MyPlacesApp/ViewController.swift
+++ MyPlacesApp/ViewController.swift
@@ -9,36 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let arrayOfCafe = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
+    var places = Place.getPlaces()
     
     //    MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
     }
 }
 
 // MARK: DataSource to VC
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrayOfCafe.count
+        return places.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseCell") as? CustomTableViewCell else { return UITableViewCell() }
-        cell.nameLabel?.text = arrayOfCafe[indexPath.row]
-        cell.imageOfPlace?.image = UIImage(named: arrayOfCafe[indexPath.row])
+        cell.nameLabel?.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].locaction
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.imageOfPlace?.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace?.clipsToBounds = true
         return cell
     }
-    
-    
-//    MARK: Delegate
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 85
-    }
-    
 }
 
