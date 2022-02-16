b529bfdfb19ffb4115f68f8bbff4477f2449084d
Belverk070 <vometlin@gmail.com>
2022-02-16 18:32:35 +0300

    Added array of Cafes for tests and commit it

--- MyPlacesApp/ViewController.swift
+++ MyPlacesApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let arrayOfCafe = ["Farsh", "KFC", "Lala-land", "Saperavi", "True Cost"]
+    
+    //    MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
+}
 
-
+// MARK: Extenstion to VC
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayOfCafe.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseCell") else { return UITableViewCell() }
+        cell.textLabel?.text = arrayOfCafe[indexPath.row]
+        return cell
+    }
 }
 
