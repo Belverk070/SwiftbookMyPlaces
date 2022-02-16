6d648644f167b8d3db86e01bbee3cc7fe196ec08
Belverk070 <vometlin@gmail.com>
2022-02-16 18:48:51 +0300

    Added pictures

--- MyPlacesApp/ViewController.swift
+++ MyPlacesApp/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let arrayOfCafe = ["Farsh", "KFC", "Lala-land", "Saperavi", "True Cost"]
+    let arrayOfCafe = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
     
     //    MARK: ViewDidLoad
     override func viewDidLoad() {
@@ -27,6 +27,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseCell") else { return UITableViewCell() }
         cell.textLabel?.text = arrayOfCafe[indexPath.row]
+        cell.imageView?.image = UIImage(named: arrayOfCafe[indexPath.row])
         return cell
     }
 }
