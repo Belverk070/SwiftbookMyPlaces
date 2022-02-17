d4681ba64d81bdb7618fa0001eedd3057a29a20d
Belverk070 <vometlin@gmail.com>
2022-02-17 17:37:56 +0300

    Added ImagePickerController for NewPlaceVC

--- MyPlacesApp/NewPlaceViewController.swift
+++ MyPlacesApp/NewPlaceViewController.swift
@@ -12,17 +12,35 @@ class NewPlaceViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        убрали разлиновку заменив его обычным view
+        //        убрали разлиновку заменив его обычным view
         tableView.tableFooterView = UIView()
         
     }
     
-//    MARK: TableViewDelegate
+    //    MARK: TableViewDelegate
     
-//    настроили скрытие клавиатуры при нажатии на любую ячейку, кроме первой
+    //    настроили скрытие клавиатуры при нажатии на любую ячейку, кроме первой
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImagePicker(source: .camera)
+            }
+            
+            let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImagePicker(source: .photoLibrary)            }
+            
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(camera)
+            actionSheet.addAction(photo)
+            actionSheet.addAction(cancel)
+            
+            present(actionSheet, animated: true)
+            
             
         } else {
             view.endEditing(true)
@@ -33,9 +51,25 @@ class NewPlaceViewController: UITableViewController {
 // MARK: TextFieldDelegate
 extension NewPlaceViewController: UITextFieldDelegate {
     
-//    скрываем клавиатуру по нажатию на Done
+    //    скрываем клавиатуру по нажатию на Done
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
+}
+
+//MARK: Work with image
+
+extension NewPlaceViewController {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        //        проверяем источник выбора на доступность
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
 }
