8f057d0aac51c1f74d0f35308eaf38d3cec938d4
Belverk070 <vometlin@gmail.com>
2022-02-18 21:26:49 +0300

    Added filtration and searching for VC

--- MyPlacesApp/ViewController.swift
+++ MyPlacesApp/ViewController.swift
@@ -10,9 +10,19 @@ import RealmSwift
 
 class ViewController: UIViewController {
     
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var filteredPlaces: Results<Place>!
+    private var searchBarIsEmpty: Bool {
+        guard let text = searchController.searchBar.text else { return false }
+        return text.isEmpty
+    }
+    private var isFiltering: Bool {
+        return searchController.isActive && !searchBarIsEmpty
+    }
+    
     //    Results - массив из Realm с данными
-    var places: Results<Place>!
-    var ascendingSorting = true
+    private var places: Results<Place>!
+    private var ascendingSorting = true
     
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var reversedSortedButoon: UIBarButtonItem!
@@ -23,6 +33,13 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         //        добавляем данные из реалма в массив places
         places = realm.objects(Place.self)
+        
+        //        Setup the search controller
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
         
     }
     @IBAction func sortSelection(_ sender: UISegmentedControl) {
@@ -42,10 +59,8 @@ class ViewController: UIViewController {
     private func sorting() {
         if segmentedControl.selectedSegmentIndex == 0 {
             places = places.sorted(byKeyPath: "date", ascending: ascendingSorting)
-            
         } else {
             places = places.sorted(byKeyPath: "name", ascending: ascendingSorting)
-            
         }
         myPlacesTableView.reloadData()
     }
@@ -55,13 +70,21 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     // MARK: DataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            return filteredPlaces.count
+        }
         return places.isEmpty ? 0 : places.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseCell") as? CustomTableViewCell else { return UITableViewCell() }
+        var place = Place()
+        if isFiltering {
+            place = filteredPlaces[indexPath.row]
+        } else {
+            place = places[indexPath.row]
+        }
         
-        let place = places[indexPath.row]
         cell.nameLabel?.text = place.name
         cell.locationLabel.text = place.locaction
         cell.typeLabel.text = place.type
@@ -69,6 +92,7 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         cell.imageOfPlace.image = UIImage(data: place.imageData!)
         cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace?.clipsToBounds = true
+        
         return cell
     }
     
@@ -103,7 +127,12 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             guard let indexPath = myPlacesTableView.indexPathForSelectedRow else { return }
-            let place = places[indexPath.row]
+            let place: Place
+            if isFiltering {
+                place = filteredPlaces[indexPath.row]
+            } else {
+                place = places[indexPath.row]
+            }
             let newPlaceVC = segue.destination as! NewPlaceViewController
             newPlaceVC.currentPlace = place
         }
@@ -119,3 +148,14 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
 }
 
+//MARK: UISearchResultsUpdating
+extension ViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+    
+    private func filterContentForSearchText(_ searchText: String) {
+        filteredPlaces = places.filter("name CONTAINS[c] %@ OR locaction CONTAINS[c] %@", searchText, searchText)
+        myPlacesTableView.reloadData()
+}
+}
