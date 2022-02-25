4fe790e8bd11d6cb0088ecc29b5457a8a914c36c
Belverk070 <vometlin@gmail.com>
2022-02-25 20:12:51 +0300

    Added MapKit to project

--- MyPlacesApp/ViewController.swift
+++ MyPlacesApp/ViewController.swift
@@ -73,25 +73,20 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         if isFiltering {
             return filteredPlaces.count
         }
-        return places.isEmpty ? 0 : places.count
+        return places.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "reuseCell") as? CustomTableViewCell else { return UITableViewCell() }
-        var place = Place()
-        if isFiltering {
-            place = filteredPlaces[indexPath.row]
-        } else {
-            place = places[indexPath.row]
-        }
+        
+        let place = isFiltering ? filteredPlaces[indexPath.row] : places[indexPath.row]
+        
         
         cell.nameLabel?.text = place.name
         cell.locationLabel.text = place.locaction
         cell.typeLabel.text = place.type
         //        добавляем логику по изображению заведения
         cell.imageOfPlace.image = UIImage(data: place.imageData!)
-        cell.imageOfPlace?.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
-        cell.imageOfPlace?.clipsToBounds = true
         
         return cell
     }
@@ -114,29 +109,12 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    //    можно использовать для настройки массива действий по свайпу справа налево
-    //    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-    //
-    //        let place = places[indexPath.row]
-    //        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { _, _, _ in
-    //            StorageManager.deleteObject(place)
-    //            tableView.deleteRows(at: [indexPath], with: .automatic)
-    //        }
-    //
-    //        return UISwipeActionsConfiguration(actions: [deleteAction])
-    //    }
-    
     // MARK: - Navigation
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showDetail" {
             guard let indexPath = tableView.indexPathForSelectedRow else { return }
-            let place: Place
-            if isFiltering {
-                place = filteredPlaces[indexPath.row]
-            } else {
-                place = places[indexPath.row]
-            }
+            let place = isFiltering ? filteredPlaces[indexPath.row] : places[indexPath.row]
             let newPlaceVC = segue.destination as! NewPlaceViewController
             newPlaceVC.currentPlace = place
         }
@@ -161,5 +139,5 @@ extension ViewController: UISearchResultsUpdating {
     private func filterContentForSearchText(_ searchText: String) {
         filteredPlaces = places.filter("name CONTAINS[c] %@ OR locaction CONTAINS[c] %@", searchText, searchText)
         tableView.reloadData()
-}
+    }
 }
